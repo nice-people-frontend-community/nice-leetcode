ac68f813d6b39c0be093acbd203ebc8570ee6042
Anonymity94 <1176989591@qq.com>
2022-06-19 16:11:10 +0800

    fix: routes

--- docs/assets/Ranking.fc118a19.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index.a908ca3d.js";const r={};function a(o,t){return c(),n("h2",null,"\u603B\u699C")}var s=e(r,[["render",a]]);export{s as default};
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const routes = [
   { path: '/', redirect: '/daily' },
   { path: '/daily', component: () => import('../views/Daily.vue') },
   { path: '/weekly', component: () => import('../views/Weekly.vue') },
-  { path: '/first-class', component: () => import('../views/Ranking.vue') },
+  { path: '/first-class', component: () => import('../views/FirstClass.vue') },
   { path: '/ranking', component: () => import('../views/Ranking.vue') },
   { path: '/:routeKey(.*)', redirect: '/daily' }, // 兜底跳转
 ];
