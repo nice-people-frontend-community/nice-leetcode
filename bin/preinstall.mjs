1180b5246d7de0bbdc86bc280d4b0bcf98dcd684
Sneaken <924393527@qq.com>
2022-06-21 23:11:49 +0800

    refactor: 💡 probably should use npm_config_user_agent

--- bin/preinstall.mjs
+++ bin/preinstall.mjs
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
-const npmExecPath = process.env.npm_execpath || '';
-if (!/yarn/.test(npmExecPath)) {
+const packageManagerUA = process.env.npm_config_user_agent || '';
+if (!/yarn/.test(packageManagerUA)) {
   console.warn(
     `\u001b[33mThis repository requires using yarn as the package manager ` +
       `for scripts to work properly.\u001b[39m\n`,
@@ -8,9 +8,9 @@ if (!/yarn/.test(npmExecPath)) {
 
   console.warn(`\u001b[33mYou should run the following script to reinstall dependencies.\u001b[39m\n`);
   console.warn(`\u001b[33mrm -rf ./node_modules\u001b[39m`);
-  if (/pnpm/.test(npmExecPath)) {
+  if (/pnpm/.test(packageManagerUA)) {
     console.warn(`\u001b[33mrm -rf ./pnpm-lock.yaml\u001b[39m`);
-  } else if (/npm/.test(npmExecPath)) {
+  } else if (/npm/.test(packageManagerUA)) {
     console.warn(`\u001b[33mrm -rf ./package-lock.json\u001b[39m`);
   }
   console.warn(`\u001b[33myarn install\u001b[39m\n`);
