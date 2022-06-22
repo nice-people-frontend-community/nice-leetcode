aa073bf0f095830ca9e2e067dad396da35367547
Sneaken <924393527@qq.com>
2022-06-23 00:11:36 +0800

    refactor: 💡 where does the eslint config point to?

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,17 @@ module.exports = defineConfig({
     browser: true,
   },
   extends: [
+    // node_modules/eslint-plugin-vue/lib/configs/vue3-essential
     'plugin:vue/vue3-essential',
+    // node_modules/eslint/conf/eslint-recommended.js
     'eslint:recommended',
+    // node_modules/@vue/eslint-config-typescript/index.js => node_modules/@typescript-eslint/dist/configs/eslint-recommended.js
     '@vue/eslint-config-typescript/recommended',
+    // node_modules/@vue/eslint-config-prettier/index.js => node_modules/eslint-plugin-prettier/eslint-plugin-prettier.js
     '@vue/eslint-config-prettier',
   ],
   rules: {
+    // 下面的配置能够覆盖之前的配置
     'vue/multi-word-component-names': 'off',
   },
 });
