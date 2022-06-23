e15360fbc8ce4dd4332350f4cac579a6077d92b0
Sneaken <924393527@qq.com>
2022-06-24 00:36:19 +0800

    refactor: 自动导入 vue 和 vue-router 的 api

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
+      imports: ['vue', 'vue-router'],
       resolvers: [ElementPlusResolver()],
     }),
     Components({
