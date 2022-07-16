cbea27e7caaa80f99e3298a6008538de3b807d30
Anonymity94 <1176989591@qq.com>
2022-07-16 15:01:09 +0800

    fix: 23:55 ~ 00:00 之间丢数据

--- src/ac_record.ts
+++ src/ac_record.ts
@@ -13,9 +13,10 @@ const yesterday = dayjs().subtract(1, 'day').format(DATE_FORMAT_STRING);
 
 // 由于 GitHub Actions 定时器启动不准，这里做下兼容处理
 // 定时器不会延迟太久，所以这里仅判断跨天的的即可
-// 如果启动时间在凌晨 00:30 以内的话，就查询昨天记录
+// 如果启动时间在凌晨 02:00 以内的话，就查询昨天记录
+// 防止前一天丢数据
 let queryDate = today;
-if (new Date().getHours() === 0 && new Date().getMinutes() < 30) {
+if (new Date().getHours() < 2) {
   queryDate = yesterday;
 }
 
