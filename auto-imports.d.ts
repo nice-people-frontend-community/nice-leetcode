fe95f817406fd3af2d898038e353e8e82fb9ba35
Sneaken <924393527@qq.com>
2022-07-19 17:26:20 +0800

    fix: auto-import 感觉有点问题

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -2,6 +2,7 @@
 export {};
 declare global {
   const EffectScope: typeof import('vue')['EffectScope'];
+  const ElMessage: typeof import('element-plus/es')['ElMessage'];
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed'];
   const autoResetRef: typeof import('@vueuse/core')['autoResetRef'];
   const axios: typeof import('axios')['default'];
