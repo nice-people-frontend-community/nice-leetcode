32d393e932643e921e977f810115e4a46bdf4c53
Sneaken <924393527@qq.com>
2022-07-20 16:34:23 +0800

    chore: 调整添加人员的脚本

--- scripts/add_user.ts
+++ scripts/add_user.ts
@@ -1,5 +1,7 @@
 import { getAcSubmissions, getToday, getWeekStartAndEnd } from './utils';
 import type { IUser } from './typings';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const today = getToday();
 const weekDateList = getWeekStartAndEnd(today);
@@ -9,9 +11,12 @@ const newUsers: IUser[] = [];
 // 其实可以 getAcSubmissions() 修改成直接扫描当前可以获取到的全部记录，重新记录
 // 懒得改了...
 (async () => {
+  const userFilePath = path.resolve(__dirname, `../data/common/user.json`);
+  const userJSON = fs.readFileSync(userFilePath, { encoding: 'utf-8' });
+  const users = JSON.parse(userJSON);
   for (let i = 0; i < newUsers.length; i++) {
     const user = newUsers[i];
-
+    users.push(user);
     for (let j = 0; j < weekDateList.length; j++) {
       const date = weekDateList[j];
       await getAcSubmissions(user, date);
@@ -20,4 +25,5 @@ const newUsers: IUser[] = [];
       }
     }
   }
+  fs.writeFileSync(userFilePath, JSON.stringify(users, null, 2), { encoding: 'utf-8' });
 })();
