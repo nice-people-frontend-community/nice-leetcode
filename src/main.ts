62190a5487190043890fd759dfa1dfcd24dcfa8a
Anonymity94 <1176989591@qq.com>
2022-08-11 09:07:44 +0800

    fix: async

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ import type { QuestionMap } from '@@/scripts/typings';
 
 const app = createApp(App);
 
-const { data: quertionMapRes } = await axios.get<QuestionMap>(`/data/common/all_questions_map.json?v=${+new Date()}`);
-app.config.globalProperties.$quertionMap = quertionMapRes;
+(async () => {
+  const { data: quertionMapRes } = await axios.get<QuestionMap>(`/data/common/all_questions_map.json?v=${+new Date()}`);
+  app.config.globalProperties.$quertionMap = quertionMapRes;
+})();
 
 app.use(router);
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
