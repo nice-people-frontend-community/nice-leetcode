37a8fc2fc411611fa2532eaee73f4d5e4c331ccb
Anonymity94 <1176989591@qq.com>
2022-08-11 09:21:13 +0800

    feat: cache questions

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-import './utils/axios';
+import type { QuestionMap } from '@@/scripts/typings';
+import HighchartsVue from 'highcharts-vue';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import HighchartsVue from 'highcharts-vue';
-import type { QuestionMap } from '@@/scripts/typings';
+import './utils/axios';
 
 const app = createApp(App);
 
-(async () => {
-  const { data: quertionMapRes } = await axios.get<QuestionMap>(`/data/common/all_questions_map.json?v=${+new Date()}`);
-  app.config.globalProperties.$quertionMap = quertionMapRes;
-})();
+// 记录到 storage 中缓存起来
+const localQuertionMap: string | null = window.localStorage.getItem('leetcode_questions');
+if (localQuertionMap) {
+  app.config.globalProperties.$quertionMap = JSON.parse(localQuertionMap);
+} else {
+  (async () => {
+    const { data: quertionMapRes } = await axios.get<QuestionMap>(`/data/common/all_questions_map.json`);
+    app.config.globalProperties.$quertionMap = quertionMapRes;
+    window.localStorage.setItem('leetcode_questions', JSON.stringify(quertionMapRes));
+  })();
+}
 
 app.use(router);
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
